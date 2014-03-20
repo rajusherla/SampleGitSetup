a4bb83b64353db9c8daf909253f3ada597a03738
rajus <raju.sai12@gmail.com>
2014-03-20 12:32:43 -0500

    initial commit

---
+++ SampleGitSetup/src/main/java/com/bizlab/SampleGitSetup/App.java
@@ -0,0 +1,13 @@
+package com.bizlab.SampleGitSetup;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
